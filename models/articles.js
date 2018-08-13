ce41125d2cba65efcdefc67c6505ee7da0835e54
Emily McKenna <emilymckenna08@gmail.com>
2018-08-12 23:11:53 -0400

    adding articles to database by clicking worthy button

--- models/articles.js
+++ models/articles.js
@@ -1,19 +1,52 @@
 module.exports = function(sequelize, DataTypes) {
-  var Articles = sequelize.define("articles", {
-    articleUrl: {
+  var Article = sequelize.define("Article", {
+    publication: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    articleHeadline: {
+    url: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    upNumber: {
-      type: DataTypes.INTEGER
+    headline: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    section: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    subsection: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    byline: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
     },
     articleImg: {
       type: DataTypes.STRING
+    },
+    articleImgLg: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    worthyScore: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
   });
-  return Articles;
+  return Article;
 };
--- models/user.js
+++ models/user.js
@@ -1,10 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("user", {
-    userName: {
+    userNameFirst: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userNameLast: {
       type: DataTypes.STRING,
       allowNull: false
     },
     userEmail: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userPassword: {
       type: DataTypes.STRING,
       allowNull: false
     }
