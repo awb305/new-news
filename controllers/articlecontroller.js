2300ff56b4c380b48e0384702e35ec9a900f4c2b
DMilligan <dmilligan@springlocker.com>
2018-08-16 13:09:15 -0400

    switching save article route to articlecontroller.js

--- controllers/articlecontroller.js
+++ controllers/articlecontroller.js
@@ -1,46 +1,184 @@
+// ==============================================================================
+// Set Dependencies & Required files
+// ==============================================================================
+
+var db = require("../models");
+
+// ==============================================================================
+// Export Functions
+// ==============================================================================
+
 var exports = (module.exports = {});
 
-// exports.signup = function(req, res) {
-//   var displayObj = {
-//     title: "News Worthy Sign-Up"
-//   };
-//   res.render("sign-up", displayObj);
-// };
+exports.saveArticle = function(req, res) {
+  // first step is to see if the article exists
+  db.Article.findOne({
+    where: {
+      url: req.body.url
+    }
+  }).then(function(articleResults) {
+    // check if the article exists
+    if (articleResults === null) {
+      // article does not exist, add to database
+      db.Article.create({
+        publication: req.body.publication,
+        url: req.body.url,
+        headline: req.body.headline,
+        section: req.body.section,
+        subsection: req.body.subsection,
+        title: req.body.title,
+        byline: req.body.byline,
+        summary: req.body.summary,
+        date: req.body.date,
+        articleImg: req.body.image,
+        articleImgLg: req.body.imageLarge,
+        worthyScore: 0
+      }).then(function(articleResponse) {
+        // store the new article ID as a string
+        var articleId = articleResponse.id;
 
-// exports.login = function(req, res) {
-//   var displayObj = {
-//     title: "News Worthy Log In"
-//   };
-//   res.render("log-in", displayObj);
-// };
+        // article has been created, check if the user has a Saved bundle
+        db.Bundle.findOne({
+          where: {
+            userid: req.body.userId,
+            name: req.body.bundle
+          }
+        }).then(function(bundleResults) {
+          // check if the user has the bundle
+          if (bundleResults === null) {
+            // bundle does not exist, create it and add the article
+            db.Bundle.create({
+              userid: req.body.userId,
+              description: "",
+              name: req.body.bundle,
+              articles: articleId
+            }).then(function(bundleResponse) {
+              // path ends, respond to user
+              res.json(bundleResponse);
+            });
+          } else {
+            // bundle does exist, add the article
+            // get the current list of articles in the bundle
+            var bundleArticles = bundleResults.articles;
+            // add the article
+            if (bundleArticles === "") {
+              bundleArticles += articleId;
+            } else {
+              bundleArticles += ", " + articleId;
+            }
+            // update the bundle in the database to include the new article
+            db.Bundle.update(
+              {
+                articles: bundleArticles
+              },
+              {
+                where: {
+                  userid: req.body.userId,
+                  name: req.body.bundle
+                }
+              }
+            ).then(function(bundleResponse) {
+              //path ends, respond to user
+              res.json(bundleResponse);
+            });
+          }
+        });
+      });
+    } else {
+      // article does exist
+      // store the article id
+      var articleId = articleResults.id;
+      // check if the user has a Saved bundle
+      db.Bundle.findOne({
+        where: {
+          userid: req.body.userId,
+          name: req.body.bundle
+        }
+      }).then(function(bundleResults) {
+        // check if the user has the bundle
+        if (bundleResults === null) {
+          // bundle does not exist, create it and add the article
+          db.Bundle.create({
+            userid: req.body.userId,
+            description: "",
+            name: req.body.bundle,
+            articles: articleId
+          }).then(function(bundleResponse) {
+            // path ends, respond to user
+            res.json(bundleResponse);
+          });
+        } else {
+          // article exists and user has Saved bundle
 
-// exports.dashboard = function(req, res) {
-//   res.render("dashboard-test");
-// };
+          // store the articles in the bundle
+          var bundleArticles = bundleResults.articles;
+          console.log(bundleArticles);
 
-// exports.logout = function(req, res) {
-//   req.session.destroy(function(err) {
-//     if (err) {
-//       throw err;
-//     }
-//     res.redirect("/");
+          // extract the list of articles to an array
+          var bundleArticlesArr = bundleArticles.split(", ");
+          console.log(bundleArticlesArr);
+          console.log("articleId: " + articleId);
+
+          // set a variable determining if the article is in the array to false
+          var articleIncluded = false;
+
+          // sort through the array and see if the article is in it
+          for (var i = 0; i < bundleArticlesArr.length; i++) {
+            // convert the item to an integer
+            var parsedId = parseInt(bundleArticlesArr[i]);
+
+            // if the article is in the array, break the for loop and respond to user
+            if (parsedId === articleId) {
+              articleIncluded = true;
+              break;
+            }
+          }
+
+          // if the article is already in the bundle
+          if (articleIncluded) {
+            // path ends, respond to user
+            res.json(bundleResults);
+          } else {
+            // article is not in the bundle, add the article to the array
+            bundleArticlesArr.push(articleId);
+
+            // join the array back together
+            var updatedArticles = bundleArticlesArr.join(", ");
+
+            // update the bundle in the database
+            db.Bundle.update(
+              {
+                articles: updatedArticles
+              },
+              {
+                where: {
+                  userid: req.body.userId,
+                  name: req.body.bundle
+                }
+              }
+            ).then(function(bundleResponse) {
+              //path ends, respond to user
+              res.json(bundleResponse);
+            });
+          }
+        }
+      });
+    }
+  });
+};
+
+// ==============================================================================
+// Database Functions
+// ==============================================================================
+
+// function createBundle(bundleData, articleId) {
+//   db.Bundle.create({
+//     userid: bundleData.userId,
+//     description: "",
+//     name: bundleData.bundle,
+//     articles: articleId
+//   }).then(function(bundleResponse) {
+//     // path ends, respond to user
+//     res.json(bundleResponse);
 //   });
-// };
-
-// exports.userData = function(req, res) {
-//   var userData;
-//   try {
-//     userData = {
-//       id: req.user.id,
-//       firstName: req.user.userNameFirst,
-//       lastName: req.user.userNameLast
-//     };
-//   } catch (err) {
-//     userData = {
-//       id: "",
-//       firstName: "",
-//       lastName: ""
-//     };
-//   }
-//   res.json(userData);
-// };
+// }
